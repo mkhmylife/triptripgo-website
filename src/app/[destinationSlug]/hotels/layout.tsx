c58da32e168b455c94ee7f4ca01a925e45818044
Edward Mok <mokkahei24@gmail.com>
2024-01-07 04:22:14 +0800

    - updated seo title

---
+++ src/app/[destinationSlug]/hotels/layout.tsx
@@ -0,0 +1,18 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: '深圳精選酒店',
+  description: '精選深圳南山區、福田區、羅湖區十大酒店推介。幫你搜羅至 hit 心水酒店，幫你比較多間酒店價錢，幫你蒐集多間酒店真實住後評價！',
+}
+
+export default function HotelsLayout({
+                                       children,
+                                     }: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ import Gtm from "@/components/Gtm";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'TripTripGo 精選推介北上深圳酒店住宿',
+  title: {
+    template: '%s | TripTripGo 精選推介北上深圳酒店住宿',
+    default: 'TripTripGo 精選推介北上深圳酒店住宿',
+  },
   description: '精選深圳南山區、福田區、羅湖區十大酒店推介。幫你搜羅至 hit 心水酒店，幫你比較多間酒店價錢，幫你蒐集多間酒店真實住後評價！',
 }
 
